768b17a2c64736aa54081a0108d696dc770d458b
Jamili88 <develop.master643880@gmail.com>
2021-09-06 14:36:12 -0700

    Add additional unit tests to verify view functions

--- test/adapters/LidoAdapter.behavior.ts
+++ test/adapters/LidoAdapter.behavior.ts
@@ -8,6 +8,7 @@ import { ILido } from "../../typechain/ILido";
 chai.use(solidity);
 
 export function shouldBehaveLikeLidoAdapter(token: string, pool: PoolItem): void {
+  const burnAddress = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
   it(`should deposit ${token} for stETH, and be able to sell stETH in exchange for ${token}`, async function () {
     // Instance of Lido Contract
     const lidoContract: ILido = await hre.ethers.getContractAt("ILido", pool.pool) as ILido;
@@ -74,11 +75,46 @@ export function shouldBehaveLikeLidoAdapter(token: string, pool: PoolItem): void
 
   it(`should return the LDO token address (possible future reward token) and indicate staking is not possible.`, async function () {
         // The address argument does not actually matter
-        const ldoGovernanceToken = await this.lidoAdapter.getRewardToken("0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE");
+        const ldoGovernanceToken = await this.lidoAdapter.getRewardToken(burnAddress);
         expect(ldoGovernanceToken).to.be.eq("0x5A98FcBEA516Cf06857215779Fd812CA3beF1B32");
 
         // Staking should not be possible
-        const isStakingAllowed = await this.lidoAdapter.canStake("0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE");
+        const isStakingAllowed = await this.lidoAdapter.canStake(burnAddress);
         expect(isStakingAllowed).to.be.eq(false);
   });
+
+  it(`should verify that view function calls are operating correctly`, async function () {
+    // Instance of Lido Contract
+    const lidoContract: ILido = await hre.ethers.getContractAt("ILido", pool.pool) as ILido;
+    // arguments passed in are not used
+    const underlyingTokenAddress = await this.lidoAdapter.getUnderlyingTokens(burnAddress, burnAddress);
+    expect(underlyingTokenAddress[0]).to.be.eq(burnAddress);
+
+    // Let's test to see if our adapter is able to map underlying tokens to LP tokens properly
+    // Let's assume 10 ETH as our underlying token value
+    const amountInEther = hre.ethers.utils.parseEther("10");
+    const expectedLPTokenAmount = await lidoContract.getSharesByPooledEth(amountInEther);
+    const actualLPTokenAmount = await this.lidoAdapter.calculateAmountInLPToken(burnAddress, burnAddress, amountInEther);
+    expect(actualLPTokenAmount).to.be.eq(expectedLPTokenAmount);
+
+    // Let's see if our calculateRedeemableLPTokenAmount is working as intended
+    const expectedRedeemableUnderlying = await lidoContract.getPooledEthByShares(expectedLPTokenAmount);
+    const actualRedeemableUnderlying = await this.lidoAdapter.calculateRedeemableLPTokenAmount(burnAddress, burnAddress, burnAddress, expectedLPTokenAmount);
+    expect(actualRedeemableUnderlying).to.be.eq(expectedRedeemableUnderlying);
+
+    // Let's try to redeem 1 more share (LP Token) than we actually have
+    const currentBalanceOfLPTokens = await lidoContract.sharesOf(this.testDeFiAdapter.address);
+    const oneMoreThanCurrentLPTokenBalance = currentBalanceOfLPTokens.add(1);
+    const isRedeemableAmountOfSharesSufficient = await this.lidoAdapter.isRedeemableAmountSufficient(this.testDeFiAdapter.address, burnAddress, lidoContract.address, oneMoreThanCurrentLPTokenBalance);
+    expect(isRedeemableAmountOfSharesSufficient).to.be.eq(false);
+
+    // Make sure that our adapter is reading in the totalSupply correctly
+    const expectedAmountOfTotalSupply = await lidoContract.totalSupply();
+    const actualAmountOfTotalSupply = await this.lidoAdapter.getPoolValue(burnAddress, burnAddress);
+    expect(expectedAmountOfTotalSupply).to.be.eq(actualAmountOfTotalSupply);
+
+    const liquidityPoolAddress = await this.lidoAdapter.getLiquidityPoolToken(burnAddress, burnAddress);
+    expect(liquidityPoolAddress).to.be.eq("0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84");
+  });
+
 }
